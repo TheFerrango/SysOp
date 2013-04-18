ab7fac696b4d4c663baa90cb98f02ac6cabbcac8
les <nanotrippolo@gmail.com>
2013-04-18 10:48:16 +0200

    added some functions

--- main.c
+++ main.c
@@ -1,34 +1,58 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
+#include <syslog.h>
+#include <pthread.h>
+#include <fcntl.h>
 
 #define MAX_LENGTH 256
 
 char* get_xor(char*,char*);
-void tr_read();
+void* tr_read();
+void read_random(char *s);
+
+
+char text[MAX_LENGTH] = "\0";
+pthread_mutex_t lock;
 
 int main()
 {
 	//char *x = get_xor("Testo di prova ","df546tgfg435trf");
 	//printf("%s\n",x);
-	tr_read();
+	pthread_t tw,tr,td,te;
+
+    if (pthread_mutex_init(&lock, NULL) != 0)
+    {
+        printf("\n mutex init failed\n");
+        return 1;
+    }
+
+    //tr = pthread_create(&tr,NULL,tr_read,NULL);
+    char t[2];
+    read_random(t);
+    printf("%s\n", t);
+	pthread_mutex_destroy(&lock);
 	return 0;
 }
-void tr_read()
+void *tr_read()
 {
-	char text[MAX_LENGTH] = "\0";
+	
     int i = 0;
 	do
 	{
+		pthread_mutex_lock(&lock);
 		char c =getchar();
+
 		if(c != '\n')
 		{
 			text[i % MAX_LENGTH] = c;
 			i++;
 		}
 		else
+		{
 			printf("%s\n", text);
-
+			pthread_mutex_unlock(&lock);
+		}
 	}
 	while(strstr(text,"quit") == NULL);
 }
@@ -41,6 +65,29 @@ char* get_xor(char *r,char *s)
 		value[i] = s[i] ^ r[i];
 
 	return value;
-	
 }
+void read_random(char *s)
+{
+	char *tmp = (char*)malloc(strlen(s) * sizeof(char));
+	int n_bytes = strlen(s),n_read = 0;
+
+	int random_fd = open("/dev/random",O_RDONLY);
+
+	if(random_fd == -1)
+	{
+		perror("Error opening /dev/random file descriptor");
+		exit(1);
+	}
+
+	while(n_read < n_bytes)
+	{
+		n_read += read(random_fd,tmp,n_bytes);
+		printf("i read %i bytes\n",n_read);
+	}
+
+	close(random_fd);
+	strcpy(s,tmp);
+	free(tmp);
+}
+
 
