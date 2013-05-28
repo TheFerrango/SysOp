818cdcfc06edba59b7f26a4c14fad2f220bc01c0
TheFerrango <llorenzo@outlook.com>
2013-05-28 20:34:48 +0200

    pimp logs
    fixed log in pimp way mit Zeitstempel

--- sysop/src/logs.h
+++ sysop/src/logs.h
@@ -1,1 +1,6 @@
-void logGen(char * threadName, char * logMessage);
+#define LOG_MAX_SIZE 256
+#define VERBOSE 1
+
+void log_error(char * which_log,  char * log_message);
+void log_info(char * which_log,  char * log_message);
+void log_debug(char * which_log,  char * log_message);
